98e70b50df75087c7b2374de99b1e4582b9c4341
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-24 21:41:40 +0200

    zeekr: remove unused qdcm configs
    Change-Id: Idd06132a1a058a1551668b475ebd312cbcfda68a

--- zeekr-vendor.mk
+++ zeekr-vendor.mk
@@ -3152,9 +3152,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/zeekr/proprietary/vendor/etc/display/qdcm_calib_data_mipi_mot_cmd_csot_1080p_dsc_667.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_mipi_mot_cmd_csot_1080p_dsc_667.json \
     vendor/motorola/zeekr/proprietary/vendor/etc/display/qdcm_calib_data_mipi_mot_cmd_csot_1080p_dsc_667_c8.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_mipi_mot_cmd_csot_1080p_dsc_667_c8.json \
     vendor/motorola/zeekr/proprietary/vendor/etc/display/qdcm_calib_data_mipi_mot_cmd_tianma_1080p_667.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_mipi_mot_cmd_tianma_1080p_667.json \
-    vendor/motorola/zeekr/proprietary/vendor/etc/display/qdcm_calib_data_mipi_mot_video_dummy_qhd.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_mipi_mot_video_dummy_qhd.json \
-    vendor/motorola/zeekr/proprietary/vendor/etc/display/qdcm_calib_data_nt36672e_lcd_video_mode_dsi_novatek_panel_with_DSC.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_nt36672e_lcd_video_mode_dsi_novatek_panel_with_DSC.json \
-    vendor/motorola/zeekr/proprietary/vendor/etc/display/qdcm_calib_data_nt36672e_lcd_video_mode_dsi_novatek_panel_without_DSC.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_nt36672e_lcd_video_mode_dsi_novatek_panel_without_DSC.json \
     vendor/motorola/zeekr/proprietary/vendor/etc/display/qdcm_merge_rules_mipi_mot_cmd_csot_1080p_dsc_667.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_merge_rules_mipi_mot_cmd_csot_1080p_dsc_667.json \
     vendor/motorola/zeekr/proprietary/vendor/etc/eva/facedetection/model3.dat:$(TARGET_COPY_OUT_VENDOR)/etc/eva/facedetection/model3.dat \
     vendor/motorola/zeekr/proprietary/vendor/etc/face3d/qcnn_concat_file_model-0324_2_encrypted:$(TARGET_COPY_OUT_VENDOR)/etc/face3d/qcnn_concat_file_model-0324_2_encrypted \
